4bff57252ce25e69e8a70641160993a888a2aa06
dosanho <seuchuai622000@gmail.com>
2021-05-08 21:11:19 -0700

    menu

---
+++ Menu/js/menu.js
@@ -0,0 +1,11 @@
+function hienthi(){
+    x = document.getElementById('input-moblie')
+    if(x.className === "class-input-moblie"){
+        x.className = "responsive";
+
+    }
+    else{
+        x.className = "class-input-moblie"
+
+    }
+}
